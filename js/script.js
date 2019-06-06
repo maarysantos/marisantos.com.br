4d41ec4fb8f8638473e899d008f685b447ab5288
MaarySantos <maary.santos1@gmail.com>
2019-06-06 09:41:54 -0300

    mnu ixd uncionando"

---
+++ js/script.js
@@ -0,0 +1,25 @@
+(function(){
+    var $menu = document.getElementsByName('header');
+    
+    window.addEventListener('scroll', setupNav);
+    
+    function setupNav(){
+        var posYScroll = getYscroll()
+        
+        if(posYScroll > 300 && !hasClassFx()){
+            document.body.classList.add('fx');
+        } 
+        
+        if(posYScroll <= 300 && hasClassFx()) {
+            document.body.classList.remove('fx');
+        }
+    }
+    
+    function getYscroll(){
+        return window.pageYOffset;
+    }
+    
+    function hasClassFx(){
+        return !!document.querySelector('.fx')
+    }
+})()
